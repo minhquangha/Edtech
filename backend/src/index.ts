4a523a4acb5b3be285713699c108dda282098e1b
Ha Minh Quang <minhquanghmqno1@gmail.com>
2026-08-14 16:47:22 +0700

    fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,17 @@
 import express, { type Express} from "express";
 import router from "@/routes/index.js";
 import testDB from "./utils/test-db.js";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 const app: Express = express();
 
 app.use(express.json());
 app.get("/health",testDB);
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 app.use('/',router)
 const PORT = process.env.PORT || 3000;
 
