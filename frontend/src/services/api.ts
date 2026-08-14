44209ac82ac22d5583010d5041f6fb09e33ad91f
Ha Minh Quang <minhquanghmqno1@gmail.com>
2026-08-14 17:07:12 +0700

    fix syntax

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -7,7 +7,7 @@ import type {
 } from "../types";
 
 // const API_BASE_URL = "/api";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL||"/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL||"/api";
 
 const getHeaders = (token?: string | null): HeadersInit => {
   const headers: Record<string, string> = {
