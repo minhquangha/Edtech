b37f55643ecf772dc129467d5deb8bfa6580d7e7
Ha Minh Quang <minhquanghmqno1@gmail.com>
2026-08-14 16:03:27 +0700

    add build command and fix type of gemeni api key

--- backend/src/config/gemini.ts
+++ backend/src/config/gemini.ts
@@ -1,7 +1,12 @@
 import { GoogleGenAI } from "@google/genai";
 
+
+const apiKey =  process.env.GEMINI_API_KEY;
+if (!apiKey) {
+  throw new Error("Missing GEMINI_API_KEY environment variable");
+}
 const gemini = new GoogleGenAI({
-  apiKey: process.env.GEMINI_API_KEY,
+  apiKey: apiKey,
 });
 
 export default gemini;
